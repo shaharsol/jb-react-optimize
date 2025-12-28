a732a39e2653bad9bd3cd49fe16fb0097a014a73
shaharsol <shahar@gnift.com>
2025-12-28 10:07:27 +0200

    unoptimized works

---
+++ unoptimized/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [toggle, setToggle] = useState<boolean>(true)
+
+  function changeState () {
+    setToggle(!toggle)
+  }
+  return (
+    <div className='App'>
+
+      <button onClick={changeState}>change state</button>
+      {Array.from({ length: 10 }, (_, i) => (
+          <SlowComponent
+            key={i}
+          />
+        ))}
+
+    </div>
+
+  )
+}
+
+function SlowComponent() {
+  const startTime = performance.now();
+  while (performance.now() - startTime < 100) {
+    // Burn CPU cycles
+  }
+  const showTime = performance.now();
+  // const showTime = (new Date()).toLocaleTimeString()
+  return (
+    <div className='SlowComponent'>
+      {showTime}
+    </div>
+  )
+}
+export default App
